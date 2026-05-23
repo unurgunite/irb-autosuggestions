76a3b523678433d4353f5e245b937f33fd356113
unurgunite <senpaiguru1488@gmail.com>
2026-05-23 20:35:31 +0300

    [0.1.1] Added stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-require 'irb'
+unless defined?(IRB)
+  module IRB
+    # stub for Ruby 4.0
+    @conf = {}
+    def self.conf
+      @conf
+    end
+  end
+end
+
 require 'irb/autosuggestions'
 
 RSpec.configure do |config|
